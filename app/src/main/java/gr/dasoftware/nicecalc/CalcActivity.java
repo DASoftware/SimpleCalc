0441b8f9350eb795408d797790cc015f5476e8c2
Aris <adoxakis@akroninc.net>
2016-03-31 12:06:21 +0300

    Initial commit

---
+++ app/src/main/java/gr/dasoftware/nicecalc/CalcActivity.java
@@ -0,0 +1,13 @@
+package gr.dasoftware.nicecalc;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class CalcActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calc);
+    }
+}
